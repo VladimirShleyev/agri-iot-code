709248b6d549409d9de98ffe0d40e7062cbd4769
iMissile <ishutov@gmail.com>
2017-01-17 17:38:55 +0300

    Функционал повторил, скрипт существенно упростил, авторизация теперь по RSA ключу

--- cron-scripts/weather.sh
+++ cron-scripts/weather.sh
@@ -14,21 +14,35 @@ do
 WEATHER=`curl --get "http://api.openweathermap.org/data/2.5/weather?lat=55.765232&lon=37.580322&appid=285552c0e3e33b51991e71b9e9d527dc"`
 done
 
-echo $WEATHER >> /home/iot-rus/lab/weather.log
+# дебаты насчет выявления пути размещения скрипта: http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+actual_path=$(readlink -f "${BASH_SOURCE[0]}")
+script_dir=$(dirname "$actual_path")
+log_file="${script_dir}/weather.log"
+# If you want to do it without following any symlinks, then try using realpath with option -s:
+# http://stackoverflow.com/questions/6643853/how-to-convert-in-path-names-to-absolute-name-in-a-bash-script
+data_file=$(realpath -sm "${script_dir}/../data/weather.txt")
 
-cat /home/iot-rus/lab/weather.log | grep "coord" | grep -v failed > /home/iot-rus/lab/result/Moscow-Lab/weather.txt
+echo "log file name = " ${log_file}
+echo "data file name = " ${data_file}
+
+echo $WEATHER >> $log_file
+echo $WEATHER >> $data_file
+
+cat $log_file | grep "coord" | grep -v failed > $data_file
 
 #Работа с GIT
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git config user.name "iot-rus"
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git config user.email "ilya.shutov@devoteam.com"
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git config credential.helper "cache --timeout=9999999"
+cd $(dirname ${data_file})
+
+git config user.name "iot-rus"
+git config user.email "ilya.tsas@gmail.com"
+git config credential.helper "cache --timeout=9999999"
 echo "Запровижинены параметры GIT"
 
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git add weather.txt
+git add weather.txt
 echo "Выполнена команда GIT ADD"
 
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git commit -m 'Добавлены данные о текущей погоде' -a
+cmessage="Добавлены данные о текущей погоде: $(date)"
+git commit -m "${cmessage}"
 echo "Выполнена команда GIT COMMIT"
-
-cd /home/iot-rus/lab/result/Moscow-Lab/ && /usr/bin/git push --repo https://iot-rus:F0rever%21@github.com/iot-rus/Moscow-Lab.git
+git push --repo https://iot-rus:s9qMGtH90P@github.com/iot-rus/agri-iot-data
 echo "Выполнена команда GIT PUSH"
